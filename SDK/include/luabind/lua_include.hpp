2d0a0617842af50a540e3651657356a62ef3052d
noxiossy <cari0us@outlook.com>
2022-01-14 01:42:27 +0700

    Revert "0.992"
    This reverts commit 9c4af4f6af9b433e9debd08687c822374d81af40.

--- SDK/include/luabind/lua_include.hpp
+++ SDK/include/luabind/lua_include.hpp
@@ -23,12 +23,11 @@
 #ifndef LUA_INCLUDE_HPP_INCLUDED
 #define LUA_INCLUDE_HPP_INCLUDED
 
-#include "lua.hpp"
-/*extern "C" {
+extern "C" {
 	#include <lua/lua.h>
 	#include <lua/lauxlib.h>
 	#include <lua/lualib.h>
 } // extern "C"
-*/
+
 #endif
 
