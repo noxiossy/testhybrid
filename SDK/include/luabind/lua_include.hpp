84f5138cf7e496aa1e7683438ed13f1f0c50d79b
noxiossy <cari0us@outlook.com>
2022-01-13 21:49:02 +0700

    0.94

--- SDK/include/luabind/lua_include.hpp
+++ SDK/include/luabind/lua_include.hpp
@@ -24,9 +24,9 @@
 #define LUA_INCLUDE_HPP_INCLUDED
 
 extern "C" {
-	#include <lua/lua.h>
-	#include <lua/lauxlib.h>
-	#include <lua/lualib.h>
+	#include <lua.h>
+	#include <lauxlib.h>
+	#include <lualib.h>
 } // extern "C"
 
 #endif
